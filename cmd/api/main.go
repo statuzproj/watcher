f1be36c8c616bfd58ffa69013c011eefb45a3c11
Timam <mail@timam.io>
2023-10-22 02:50:41 +0600

    interservice communication success in devpod

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("watcher is awake")
+
+	resp, err := http.Get(`http://genie:8081`)
+	if err != nil {
+		panic(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("genie responded with %d : %s", resp.StatusCode, string(body))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!")
 	})
