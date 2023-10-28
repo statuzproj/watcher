a3f29b1f90b71620de9534405779af30a75ee3e0
Timam <mail@timam.io>
2023-10-28 17:49:14 +0600

    only supporting webpage type

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,27 +1,29 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 
 func main() {
 	endpoints := getEndpoints()
+	var wg sync.WaitGroup
 
 	for _, endpoint := range endpoints {
-		switch endpoint.Target.Type {
-		case "webpage":
-			err := pageWatcher(endpoint.Name, endpoint.Target.Endpoint, endpoint.Target.Interval)
-			if err != nil {
-				log.Println(err)
+		wg.Add(1)
+
+		go func(endpoint Endpoint) {
+			defer wg.Done()
+			for {
+				switch endpoint.Target.Type {
+				case "webpage":
+					err := pageWatcher(endpoint.Name, endpoint.Target.Endpoint, endpoint.Target.Interval)
+					if err != nil {
+						log.Println(err)
+					}
+				}
 			}
-		case "ip":
-			err := ipWatcher(endpoint.Name, endpoint.Target.Endpoint, endpoint.Target.Interval)
-			if err != nil {
-				log.Println(err)
-			}
-		case "api":
-			err := apiWatcher(endpoint.Name, endpoint.Target.Endpoint, endpoint.Target.Interval)
-			if err != nil {
-				log.Println(err)
-			}
-		}
+		}(endpoint)
 	}
+	wg.Wait()
 }
--- cmd/api/watchapi.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "log"
-
-func apiWatcher(name string, endpoint string, interval string) error {
-	log.Printf(" watching %s, %s, %s", name, endpoint, interval)
-	return nil
-}
